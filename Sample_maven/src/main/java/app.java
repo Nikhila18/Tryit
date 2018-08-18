6fc600672dcab5df9faa4fa76a7336c083fc1ad3
Nikhila18 <40505478+Nikhila18@users.noreply.github.com>
2018-08-17 21:06:16 -0700

    Create app.java

---
+++ Sample_maven/src/main/java/app.java
@@ -0,0 +1,14 @@
+package sample_maven_project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
+new BufferedReader(new InputStreamReader(getClass().getResourceAsStream("/config.txt")));
